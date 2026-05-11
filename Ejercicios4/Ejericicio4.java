2d36842de5a6e3a3909097e44556f2fd129dc80a
Edu <echavezv@utea.edu.pe>
2026-05-11 09:41:55 -0500

    ejercicio4 terminado

--- Ejercicios4/Ejericicio4.java
+++ Ejercicios4/Ejericicio4.java
@@ -3,7 +3,7 @@ package Ejercicios4;
 public class Ejericicio4 {
     public static void main(String[] args) {
         System.out.println("Programa que indica tu signo");
-        
+
         System.out.print("Ingrese el mes de nacimiento (1-12): ");
         int mes = Integer.parseInt(System.console().readLine());
 
@@ -27,11 +27,79 @@ public class Ejericicio4 {
                     horoscopo = "piscis";
                 }
                 break;
+            case 3:
+                if (dia < 21) {
+                    horoscopo = "piscis";
+                } else {
+                    horoscopo = "aries";
+                }
+                break;
+            case 4:
+                if (dia < 20) {
+                    horoscopo = "aries";
+                } else {
+                    horoscopo = "tauro";
+                }
+                break;
+            case 5:
+                if (dia < 20) {
+                    horoscopo = "tauro";
+                } else {
+                    horoscopo = "géminis";
+                }
+                break;
+            case 6:
+                if (dia < 21) {
+                    horoscopo = "géminis";
+                } else {
+                    horoscopo = "cancer";
+                }
+                break;
+            case 7:
+                if (dia < 23) {
+                    horoscopo = "cancer";
+                } else {
+                    horoscopo = "leo";
+                }
+                break;
+            case 8:
+                if (dia < 24) {
+                    horoscopo = "leo";
+                } else {
+                    horoscopo = "virgo";
+                }
+                break;
+            case 9:
+                if (dia < 23) {
+                    horoscopo = "virgo";
+                } else {
+                    horoscopo = "libra";
+                }
+                break;
+            case 10:
+                if (dia < 23) {
+                    horoscopo = "libra";
+                } else {
+                    horoscopo = "escorpio";
+                }
+                break;
+            case 11:
+                if (dia < 23) {
+                    horoscopo = "escorpio";
+                } else {
+                    horoscopo = "sagitario";
+                }
+                break;
+            case 12:
+                if (dia < 21) {
+                    horoscopo = "sagitario";
+                } else {
+                    horoscopo = "capricorpnio";
+                }
+                break;
             default:
                 break;
         }
-
-
-
+        System.out.println("Tu signo del zodiaco es: " + horoscopo);
     }
 }
