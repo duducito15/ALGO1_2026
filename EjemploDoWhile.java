d504b0be72b949b1bbc7caa92979f41524d3c04e
Edu <echavezv@utea.edu.pe>
2026-05-18 09:00:56 -0500

    Estructuras repetitivas

---
+++ EjemploDoWhile.java
@@ -0,0 +1,17 @@
+public class EjemploDoWhile {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 11);
+        System.err.println("----------------------");
+        
+        int j = 10;
+        do {
+            System.out.println(j);
+            j--;
+        } while (j > 0);
+    }
+}
---
+++ EjemploWhile.java
@@ -0,0 +1,17 @@
+public class EjemploWhile {
+    public static void main(String[] args) {
+        // While, para repetir instrucciones sin un final especifico
+        
+        int i = 1;
+        while (i < 11) {
+            System.out.println(i); // del 1 al 10
+            i++;
+        }
+
+        int j = 10;
+        while (j > 0) {
+            System.out.println(j); // del 10 al 1
+            j--;
+        }
+    }
+}
