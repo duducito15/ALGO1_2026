3991741415547f4486897436d99c68c623f81539
Eduardo <echavezv@utea.edu.pe>
2026-05-21 09:36:08 -0500

    ejericicios 5

--- Ejercicios5/Ejercicio2.java
+++ Ejercicios5/Ejercicio2.java
@@ -2,6 +2,17 @@ package Ejercicios5;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+        /*
+         * Ejercicio 2
+         * Muestra los números múltiplos de 5 de 0 a 100 utilizando
+         * un bucle while.
+         */
+        int i = 0;
+        while (i < 101) {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
---
+++ Ejercicios5/Ejercicio3.java
@@ -0,0 +1,17 @@
+package Ejercicios5;
+
+public class Ejercicio3 {
+    /*
+     * Ejercicio 3
+     * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do-while.
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            if (i % 5 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        } while (i < 101);
+    }
+}
