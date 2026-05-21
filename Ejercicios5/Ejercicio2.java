f104039da31ff039ebba2c65f931fe016e2f5d6a
Eduardo <echavez@utea.edu.pe>
2026-05-21 09:22:29 -0500

    Ejercicio 5

---
+++ Ejercicios5/Ejercicio2.java
@@ -0,0 +1,7 @@
+package Ejercicios5;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Ejercicios5/Ejericicio1.java
@@ -0,0 +1,15 @@
+package Ejercicios5;
+
+public class Ejericicio1 {
+    /*
+    Ejercicio 1
+    Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+    */
+   public static void main(String[] args) {
+    for (int i = 0; i < 101; i++) {
+        if (i % 5 == 0) {
+            System.out.println(i);
+        }
+    }
+   }
+}
