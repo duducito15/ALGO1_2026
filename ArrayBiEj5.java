cc3a755b9576fa883f66d355f05fe21e5b4eb74c
Edu <echavezv@utea.edu.pe>
2026-06-15 09:56:21 -0500

    ArrrayBiEj 5

---
+++ ArrayBiEj5.java
@@ -0,0 +1,41 @@
+public class ArrayBiEj5 {
+    public static void main(String[] args) throws InterruptedException {
+
+        int[][] num = new int[6][10];
+
+        int f, c;
+
+        int minimo = 1000;
+        int maximo = 0;
+
+        int fminimo = 0;
+        int fmaximo = 0;
+        int cmaximo = 0;
+        int cminimo = 0;
+
+        for (f = 0; f < 6; f++) {
+            for (c = 0; c < 10; c++) {
+                num[f][c] = (int) (Math.random() * 900) + 100;
+                System.out.printf("%5d ", num[f][c]);
+                // Thread.sleep(500);
+
+                if (num[f][c] < minimo) {
+                    minimo = num[f][c];
+                    fminimo = f;
+                    cminimo = c;
+                }
+
+                if (num[f][c] > maximo) {
+                    maximo = num[f][c];
+                    fmaximo = f;
+                    cmaximo = c;
+                }
+            }
+            System.out.println("|");
+        }
+
+        System.out.println("El minimo: " + minimo + " y esta en [ " + fminimo + ";" + cminimo + " ]");
+        System.out.println("El maximo: " + maximo + " y esta en [ " + fmaximo + ";" + cmaximo + " ]");
+
+    }
+}
