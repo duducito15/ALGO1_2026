a16043dff42d0bd36ea6a92d31a8e6dc234ff10a
Eduardo <echavezv@utea.edu.pe>
2026-06-25 09:32:00 -0500

    calculadroa con funciones

---
+++ CalculadoraBasica.java
@@ -0,0 +1,30 @@
+public class CalculadoraBasica {
+    public static void main(String[] args) {
+        double n1, n2;
+        System.out.println("Ingresa el primer número: ");
+        n1 = Double.parseDouble(System.console().readLine());
+        System.out.println("Ingresa el segundo número: ");
+        n2 = Double.parseDouble(System.console().readLine());
+
+        System.out.println("la Suma es: " + suma(n1, n2));
+        System.out.println("la Resta es: " + resta(n1, n2));
+        System.out.println("la Multiplicación es: " + multiplicacion(n1, n2));
+        System.out.println("la Division es: " + division(n1, n2));
+    }
+
+    public static double suma(double a, double b) {
+        return a + b;
+    }
+
+    public static double resta(double x, double y) {
+        return x - y;
+    }
+
+    public static double multiplicacion(double x, double y) {
+        return x * y;
+    }
+
+    public static double division(double x, double y) {
+        return x / y;
+    }
+}
